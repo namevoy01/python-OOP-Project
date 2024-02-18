979679bdfd0bcd5150ec0d33c400af45a2e3f651
Warangkana9945 <159887705+Warangkana9945@users.noreply.github.com>
2024-02-18 20:28:06 +0700

    Update Passenger.py

--- fastapi-backend/Passenger.py
+++ fastapi-backend/Passenger.py
@@ -1,4 +1,5 @@
-class Passenger :
+from User import User
+class Passenger(User) :
     def __init__(self, account_number, email):
         self.__account_number= account_number
         self.__email = email
