e25a53ee322df818a122686b2ba7c732c9a51325
Warangkana9945 <159887705+Warangkana9945@users.noreply.github.com>
2024-02-18 18:52:53 +0700

    Create Station.py

---
+++ fastapi-backend/Station.py
@@ -0,0 +1,12 @@
+class Bus:
+    def __init__(self, station_id, name_station):
+        self.__station_id = station_id
+        self.__name_station = name_station
+
+    @property
+    def station_id(self):
+        return self.__station_id
+    
+    @property
+    def name_station(self):
+        return self.__name_station
