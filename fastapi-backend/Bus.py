e22e8f5ebb5e6dbb87a824e3a15f45303798e1ca
Warangkana9945 <159887705+Warangkana9945@users.noreply.github.com>
2024-02-18 18:29:38 +0700

    Update Bus.py

--- fastapi-backend/Bus.py
+++ fastapi-backend/Bus.py
@@ -1,2 +1,16 @@
 class Bus:
-    pass
+    def __init__(self, bus_number, location, seat_list):
+        self.__bus_number = bus_number
+        self.__location = location
+        self.__seat_list = []
+
+    @property
+    def bus_number(self):
+        return self.__bus_number
+    
+    @property
+    def location(self):
+        return self.__location
+    
+    def update_location():
+        pass
