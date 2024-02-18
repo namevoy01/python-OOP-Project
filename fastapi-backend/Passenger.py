1d98ae82b95e2c87381b27b2ce2879052db78734
Warangkana9945 <159887705+Warangkana9945@users.noreply.github.com>
2024-02-18 20:03:14 +0700

    Create Passenger.py

---
+++ fastapi-backend/Passenger.py
@@ -0,0 +1,13 @@
+class Passenger :
+    def __init__(self, account_number, email):
+        self.__account_number= account_number
+        self.__email = email
+ 
+    @property
+    def account_number(self):
+        return self.__account_number
+    
+    @property
+    def email(self):
+        return self.__email
+    
