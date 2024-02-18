f9250e7b03c8514b980ed64e0fe2281725c35ea2
Warangkana9945 <159887705+Warangkana9945@users.noreply.github.com>
2024-02-18 20:37:54 +0700

    Update BusService_Controller.py

--- fastapi-backend/BusService_Controller.py
+++ fastapi-backend/BusService_Controller.py
@@ -18,56 +18,56 @@ class BusService_Controller :
     def payment_lst(self):
         return self.__payment_lst
     
-    def get_booked_seat(self,bus_license, seat_number, seat_status):
+    def get_booked_seat(self, bus_license, seat_number, seat_status) :
         pass
 
-    def get_next_station(self,bus_license):
+    def get_next_station(self, bus_license) :
         pass
 
-    def add_province(self,province_id, province_name):
+    def add_province(self, province_id, province_name) :
         pass
 
-    def add_booking(self,booking_id, passenger_name, payment_option, amount):
+    def add_booking(self, booking_id, passenger_name, payment_option, amount) :
         pass
 
-    def add_bus(self,bus_license, seat_list, location) :
+    def add_bus(self, bus_license, seat_list, location) :
         pass
     
-    def add_route (self,source_station, destination_station, bus_trip_lst) :
+    def add_route (self, source_station, destination_station, bus_trip_lst) :
         pass
 
-    def add_passenger_to_bus(self,booking_id, schedule, seat_number) :
+    def add_passenger_to_bus(self, booking_id, schedule, seat_number) :
         pass
 
     def adjust_status_seat(self) :
         pass
 
-    def add_passenger_to_bus(self,booking_id, schedule, seat_number) :
+    def add_passenger_to_bus(self, booking_id, schedule, seat_number) :
         pass
 
-    def comfirm_booking(self,ticket_id) :
+    def comfirm_booking(self, ticket_id) :
         pass
 
-    def cancel_ticket(self,ticket_id, phone_number)  :
+    def cancel_ticket(self, ticket_id, phone_number)  :
         pass
 
-    def check_refund(self,ticket_id, phone_number) :
+    def check_refund(self, ticket_id, phone_number) :
         pass
 
-    def  send_refund_comfirm_email(self,ticket_id, email) :
+    def send_refund_comfirm_email(self, ticket_id, email) :
         pass
 
-    def search_route_by_province(self,province_id, route) :
+    def search_route_by_province(self, province_id, route) :
         pass
 
-    def search_ticket_by_ticket_id(self,ticket_id) :
+    def search_ticket_by_ticket_id(self, ticket_id) :
         pass
 
-    def search_bus_by_bus_license(self,bus_license) :
+    def search_bus_by_bus_license(self, bus_license) :
         pass
 
-    def search_station_by_province(self,province_id) :
+    def search_station_by_province(self, province_id) :
         pass
 
-    def  calculate_ticekt_price(self,distance) :
+    def  calculate_ticekt_price(self, distance) :
         pass
