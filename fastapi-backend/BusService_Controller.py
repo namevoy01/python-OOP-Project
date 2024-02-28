3f576c011334bd4656eb774132c760d2a4d06e90
fxnnieZ <chamai.rasri@gmail.com>
2024-02-28 19:58:19 +0700

    ีupdate import, class name

--- fastapi-backend/BusService_Controller.py
+++ fastapi-backend/BusService_Controller.py
@@ -1,3 +1,17 @@
+from Admin import Admin
+from Booking import Booking
+from Bus import Bus
+from BusTrip import BusTrip
+from Passenger import Passenger
+from Payment import Payment
+from Province import Province
+from Route import Route
+from Schedule import Schedule
+from Seat import Seat
+from Station import Station
+from Ticket import Ticket
+from User import User
+
 class BusService_Controller :
     def __init__(self, province_lst, ticket_lst, bus_lst, payment_lst, schedule_lst):
         self.__province_lst = []
@@ -23,7 +37,8 @@ class BusService_Controller :
         return self.__payment_lst
     
     @property
-    def get_schedule_lst
+    def get_schedule_lst(self):
+        return self.__schedule_lst
     
     def get_booked_seat(self, bus_license, seat_number, seat_status) :
         pass
