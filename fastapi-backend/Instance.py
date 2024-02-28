7fa71eed781defc89c3a9d9b9b8889712097a159
fxnnieZ <chamai.rasri@gmail.com>
2024-02-28 20:02:45 +0700

    Create Instance.py

---
+++ fastapi-backend/Instance.py
@@ -0,0 +1,16 @@
+from Admin import Admin
+from Booking import Booking
+from Bus import Bus
+from BusTrip import BusTrip
+from Passenger import Passenger
+from Payment import Payment
+from Province import Province
+from Route import Route
+from Schedule import Schedule
+from Seat import Seat
+from Station import Station
+from Ticket import Ticket
+from User import User
+
+def create_instance():
+    pass
