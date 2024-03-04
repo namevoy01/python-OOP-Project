8969e881be99f52e2d6dd927c2e962b2b85c3a42
fxnnieZ <chamai.rasri@gmail.com>
2024-03-04 13:23:53 +0700

    update payment

--- fastapi-backend/Payment.py
+++ fastapi-backend/Payment.py
@@ -5,11 +5,11 @@ class Payment:
         self.__payment_date = payment_date
         
     @property
-    def payment_id (self):
+    def get_payment_id (self):
         return self.__payment_id
     @property
-    def person_id(self):
+    def get_person_id(self):
         return self.__person_id
     @property
-    def payment_date(self):
+    def get_payment_date(self):
         return self.__payment_date
