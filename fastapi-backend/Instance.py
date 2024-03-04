abe7307a5f7be1b7b94b35a14bcdeaff0878779a
fxnnieZ <chamai.rasri@gmail.com>
2024-03-04 14:03:54 +0700

    Update Instance.py

--- fastapi-backend/Instance.py
+++ fastapi-backend/Instance.py
@@ -15,21 +15,89 @@ from BusService_Controller import BusService_Controller
 
 bus_controller = BusService_Controller([], [], [], [], [])
 
-bangkok = Province('1', 'กรุงเทพมหานคร')
+# province and route ####################################################
+
+bangkok = Province('1','กรุงเทพมหานคร')
 bus_controller.add_province(bangkok)
 
-route_1 = Route('1', 'สถานีขนส่งผู้โดยสารกรุงเทพฯ (หมอชิต 2)', 'กระบี่', 'จุดจอด อ.เหนือคลอง', 300, [])
+route_1 = Route('1',
+                'สถานีขนส่งผู้โดยสารกรุงเทพฯ (หมอชิต 2)',
+                'กรุงเทพมหานคร',
+                'ท่าอากาศยานสุวรรณภูมิ',
+                100,
+                [])
 bangkok.add_route(route_1)
 
-krabi = Province('2', 'กระบี่')
-bus_controller.add_province(krabi)
+route_2 = Route('2',
+                'สถานีขนส่งผู้โดยสารกรุงเทพฯ (หมอชิต 2)',
+                'กรุงเทพมหานคร',
+                'สถานีขนส่งผู้โดยสารกรุงเทพฯ (รังสิต)',
+                100,
+                [])
+bangkok.add_route(route_2)
 
-print("รายการจังหวัดและเส้นทาง:")
-for province in bus_controller.get_province_lst:
-    print(f"จังหวัด : {province.get_province_name}")
-    print("เส้นทาง :")
-    for route in province.get_route_lst:
-        print(f"- จุดเริ่มต้น : {route.get_source_station}")
-        print(f"  จุดหมายปลายทาง : {route.get_destination_station}")
-        print(f"  ราคา : {route.get_price} บาท")
-        break
+route_3 = Route('3',
+                'สถานีขนส่งผู้โดยสารกรุงเทพฯ (หมอชิต 2)',
+                'กระบี่',
+                'จุดจอด อ.เหนือคลอง',
+                500,
+                [])
+bangkok.add_route(route_3)
+
+route_4 = Route('4',
+                'สถานีขนส่งผู้โดยสารกรุงเทพฯ (หมอชิต 2)',
+                'กระบี่',
+                'สถานีขนส่งผู้โดยสาร จ.กระบี่',
+                500,
+                [])
+bangkok.add_route(route_4)
+
+route_5 = Route('5',
+                'สถานีขนส่งผู้โดยสารกรุงเทพฯ (หมอชิต 2)',
+                'กระบี่',
+                'จุดจอด อ.คลองท่อม',
+                500,
+                [])
+bangkok.add_route(route_5)
+
+route_6 = Route('6',
+                'สถานีขนส่งผู้โดยสารกรุงเทพฯ (หมอชิต 2)',
+                'กระบี่',
+                'ท่าเรือเกาะลันตา(บ้านหัวหิน)',
+                500,
+                [])
+bangkok.add_route(route_6)
+
+route_7 = Route('7',
+                'สถานีขนส่งผู้โดยสารกรุงเทพฯ (หมอชิต 2)',
+                'กระบี่',
+                'จุดจอด อ.เขาพนม',
+                500,
+                [])
+bangkok.add_route(route_7)
+
+krabi = Province('2', 'krabi')
+bus_controller.add_province(bangkok)
+
+# seat ####################################################
+
+bus_1 = Bus('1','1นค5463', 'on station', [])
+bus_controller.add_bus(bus_1)
+
+route_1 = Route('1',
+                'สถานีขนส่งผู้โดยสารกรุงเทพฯ (หมอชิต 2)',
+                'กรุงเทพมหานคร',
+                'ท่าอากาศยานสุวรรณภูมิ',
+                100,
+                [])
+bangkok.add_route(route_1)
+
+# print("รายการจังหวัดและเส้นทาง:")
+# for province in bus_controller.get_province_lst:
+#     print(f"จังหวัด : {province.get_province_name}")
+#     print("เส้นทาง :")
+#     for route in province.get_route_lst:
+#         print(f"- จุดเริ่มต้น : {route.get_source_station}")
+#         print(f"  จุดหมายปลายทาง : {route.get_destination_station}")
+#         print(f"  ราคา : {route.get_price} บาท")
+#         break
