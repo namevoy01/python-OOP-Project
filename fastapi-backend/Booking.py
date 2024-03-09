516d4dcce2fa5d07fec335a46819d82b4632b923
fxnnieZ <chamai.rasri@gmail.com>
2024-03-10 00:17:21 +0700

    Update Booking.py

--- fastapi-backend/Booking.py
+++ fastapi-backend/Booking.py
@@ -1,13 +1,12 @@
 class Booking:
     booking_id = 0
-    def __init__(self, name_passenger, payment_option, amount, date, payment, trip_lst):
+    def __init__(self, name_passenger, payment_option, amount, date):
         Booking.booking_id += 1
         self.__booking_id  = Booking.booking_id 
         self.__name_passenger = name_passenger
         self.__payment_option = payment_option
         self.__amount = amount
         self.__date = date
-        self.__payment = payment
         self.__bus_trip_lst = []
     
     @property
@@ -25,10 +24,11 @@ class Booking:
     @property
     def get_date(self):
         return self.__date
-    @property
-    def get_payment(self):
-        return self.__payment
     
     @property
     def get_bus_trip_lst(self):
         return self.__bus_trip_lst
+    
+    def add_bus_trip(self, bus, booking) :
+        new_booking = Booking(bus, booking)
+        self.__booking_lst.append(new_booking)
