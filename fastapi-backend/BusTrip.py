907f1193143b8c84c7b9b8d5dad46d473b47ffb8
fxnnieZ <chamai.rasri@gmail.com>
2024-03-11 10:26:52 +0700

    Update BusTrip.py

--- fastapi-backend/BusTrip.py
+++ fastapi-backend/BusTrip.py
@@ -1,17 +1,36 @@
 class BusTrip :
+<<<<<<< Updated upstream
     def __init__(self, bustrip_id , bus, schedule):
         self.__bustrip_id = bustrip_id
+=======
+<<<<<<< Updated upstream
+    bus_trip_id = 0
+    def __init__(self, bus, schedule):
+        BusTrip.bus_trip_id += 1
+        self.__bustrip_id = BusTrip.bustrip_id
+=======
+<<<<<<< Updated upstream
+    def __init__(self, bustrip_id , bus, schedule):
+        self.__bustrip_id = bustrip_id
+=======
+    bus_trip_id = 0
+    def __init__(self, bus, province):
+        BusTrip.bus_trip_id += 1
+        self.__bus_trip_id = BusTrip.bus_trip_id
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
         self.__bus = bus
-        self.__schedule = schedule
+        self.__province = province
 
     @property
-    def get_bustrip_id(self):
-        return self.__bustrip_id
+    def get_bus_trip_id(self):
+        return self.__bus_trip_id
     
     @property
     def get_bus(self):
         return self.__bus
     
     @property
-    def get_schedule(self):
-        return self.__schedule 
+    def get_province(self):
+        return self.__province 
