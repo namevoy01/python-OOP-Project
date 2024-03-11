3c8d84eb0686a3ce0a003dad2510679a816bfbf7
fxnnieZ <chamai.rasri@gmail.com>
2024-03-11 10:28:35 +0700

    Update BusTrip.py

--- fastapi-backend/BusTrip.py
+++ fastapi-backend/BusTrip.py
@@ -1,19 +1,19 @@
 class BusTrip :
     bus_trip_id = 0
-    def __init__(self, bus, schedule):
+    def __init__(self, bus, province):
         BusTrip.bus_trip_id += 1
-        self.__bustrip_id = BusTrip.bustrip_id
+        self.__bus_trip_id = BusTrip.bus_trip_id
         self.__bus = bus
-        self.__schedule = schedule
+        self.__province = province
 
     @property
-    def get_bustrip_id(self):
-        return self.__bustrip_id
+    def get_bus_trip_id(self):
+        return self.__bus_trip_id
     
     @property
     def get_bus(self):
         return self.__bus
     
     @property
-    def get_schedule(self):
-        return self.__schedule 
+    def get_province(self):
+        return self.__province 
