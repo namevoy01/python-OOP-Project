e80d5bc8cd3c06d7de0b9b288cb2b75103d44a63
namevoy01 <117102355+namevoy01@users.noreply.github.com>
2024-03-13 01:29:07 +0700

    Update navbar.js

--- react-frontend/src/component/navbar.js
+++ react-frontend/src/component/navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
 
         <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 shadow-md">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ThaiTour</span>
+            <Link to='/'><span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ThaiTour</span></Link>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-multi-level">
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
